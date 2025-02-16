d8aefc3127284cf6debae60dea1893a7fde5df7e
Harry2687 <harry.zhong2687@gmail.com>
2025-02-16 14:27:35 +0800

    converted to shiny core

--- shinyapp/app.py
+++ shinyapp/app.py
@@ -1,15 +1,20 @@
-from shiny import render, ui
+from shiny import App, render, ui
 from shiny.types import ImgData
-from shiny.express import input
 
-ui.panel_title('Image Uploader')
-ui.input_file('image', 'Image', multiple=True, accept=['.png', '.jpg', '.jpeg'])
+app_ui = ui.page_fluid(
+    ui.panel_title('Image Uploader'),
+    ui.input_file('image', 'Image', multiple=True, accept=['.png', '.jpg', '.jpeg']),
+    ui.output_image('show_image')
+)
 
-@render.image
-def render_image():
-    if input.image() is None:
-        return None
-        
-    image_path = input.image()[0]['datapath']
-    img: ImgData = {'src': image_path}
-    return img
+def server(input, output, session):
+    @render.image
+    def show_image():
+        if input.image() is None:
+            return None
+            
+        image_path = input.image()[0]['datapath']
+        img: ImgData = {'src': image_path}
+        return img
+
+app = App(app_ui, server)
