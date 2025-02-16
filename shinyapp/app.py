729507ba181c631ad2619437cb4a9f2aae4eca73
Harry2687 <harry.zhong2687@gmail.com>
2025-02-16 13:37:20 +0800

    added image upload to app

--- shinyapp/app.py
+++ shinyapp/app.py
@@ -1,10 +1,15 @@
 from shiny import render, ui
+from shiny.types import ImgData
 from shiny.express import input
 
-ui.panel_title("Hello Shiny!")
-ui.input_slider("n", "N", 0, 100, 20)
+ui.panel_title('Image Uploader')
+ui.input_file('image', 'Image', multiple=True, accept=['.png', '.jpg', '.jpeg'])
 
-
-@render.text
-def txt():
-    return f"n*2 is {input.n() * 2}"
+@render.image
+def render_image():
+    if input.image() is None:
+        return None
+        
+    image_path = input.image()[0]['datapath']
+    img: ImgData = {'src': image_path}
+    return img
