250ec14618e4ec6d11644e43a59a727669fd2595
Harry2687 <harry.zhong2687@gmail.com>
2025-02-19 19:58:47 +0800

    reorganised prediction code

--- app.py
+++ app.py
@@ -1,54 +1,11 @@
 from shiny import App, reactive, render, ui
 from shiny.types import ImgData
 
-from PIL import Image
-
-import torch
-import torchvision.transforms as transforms
-import modules.model as model
-
-def forward_prop(image_path):
-    imsize = 128
-    classes = ('Female', 'Male')
-    modelsave_name = 'model_parameters.pt'
-
-    if torch.backends.mps.is_available():
-        device = torch.device('mps')
-        device_name = 'Apple Silicon GPU'
-    elif torch.cuda.is_available():
-        device = torch.device('cuda')
-        device_name = 'CUDA'
-    else:
-        device = torch.device('cpu')
-        device_name = 'CPU'
-
-    torch.set_default_device(device)
-
-    resnet = model.resnetModel_128()
-    resnet.load_state_dict(torch.load(modelsave_name, map_location=device))
-    resnet.eval()
-
-    loader = transforms.Compose([
-        transforms.Resize([imsize, imsize]),
-        transforms.Grayscale(1),
-        transforms.ToTensor(),
-        transforms.Normalize(0, 1)
-    ])
-
-    image = Image.open(image_path).convert('RGB')
-    image_tensor = loader(image)
-    image_tensor = image_tensor.unsqueeze(0)
-
-    X = image_tensor.to(device)
-    y_pred = resnet.forward(X)
-    predicted = torch.max(y_pred.data,1)[1]
-
-    return f'Prediction: {classes[predicted]} with weight {y_pred[0][predicted].item()}. Predicted using {device_name}.'
-
+from gender_cnn.predict import predict_gender
 
 app_ui = ui.page_fluid(
-    ui.panel_title('Image Uploader'),
-    ui.input_file('image', 'Image', accept=['.png', '.jpg', '.jpeg']),
+    ui.panel_title('Gender Classifier'),
+    ui.input_file('image', 'Upload image', accept=['.png', '.jpg', '.jpeg']),
     ui.output_image('show_image'),
     ui.input_action_button('predict_gender', 'Predict'),
     ui.output_text('prediction')
@@ -61,7 +18,7 @@ def server(input, output, session):
             return None
             
         image_path = input.image()[0]['datapath']
-        img: ImgData = {'src': image_path}
+        img: ImgData = {'src': image_path, 'height': '300px', 'width': '300px'}
         return img
     
     @render.text
@@ -71,8 +28,11 @@ def server(input, output, session):
             return None
         
         image_path = input.image()[0]['datapath']
-        prediction = forward_prop(image_path)
+        output = predict_gender(image_path)
+        prediction = output['prediction']
+        weighting = output['weighting']
+        device = output['device']
 
-        return prediction
+        return f'Prediction: {prediction}. Weighting: {str(round(weighting, 2))}. Device: {device}.'
 
 app = App(app_ui, server)
---
+++ gender_cnn/predict.py
@@ -0,0 +1,47 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from .model import resnetModel_128
+
+def predict_gender(image_path: str):
+    # Constants
+    imsize = 128
+    classes = ('Female', 'Male')
+    model_name = 'resnetModel_128_epoch_2.pt'
+
+    # Set Backend
+    if torch.backends.mps.is_available():
+        device = torch.device('mps')
+        device_name = 'Apple Silicon GPU'
+    elif torch.cuda.is_available():
+        device = torch.device('cuda')
+        device_name = 'CUDA'
+    else:
+        device = torch.device('cpu')
+        device_name = 'CPU'
+
+    # Init model
+    resnet = resnetModel_128().to(device)
+    resnet.load_state_dict(torch.load(model_name, map_location=device))
+    resnet.eval()
+
+    # Load and transform image
+    loader = transforms.Compose([
+        transforms.Resize([imsize, imsize]),
+        transforms.Grayscale(1),
+        transforms.ToTensor(),
+        transforms.Normalize(0, 1)
+    ])
+
+    image = Image.open(image_path).convert('RGB')
+    image_tensor = loader(image)
+    image_tensor = image_tensor.unsqueeze(0)
+
+    # Predict
+    X = image_tensor.to(device)
+    y_pred = resnet.forward(X)
+    pred_index = torch.max(y_pred.data,1)[1]
+    prediction = classes[pred_index]
+    weighting = y_pred[0][pred_index].item()
+
+    return {'prediction': prediction, 'weighting': weighting, 'device': device_name}
